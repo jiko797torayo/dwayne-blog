0cb849d94ada853daa91005d2e7f466c5af2c919
jiko797torayo <koji@ooishikoujinoMacBook-puro.local>
2018-09-12 13:56:06 +0900

    setting-routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'article#index'
+  resources :users, only: [:index, :update, :edit]
+  resources :articles, only: [:index, :new, :create, :edit, :update]
 end
