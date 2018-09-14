9162ec7dcafbec7124d1ef5591e1f88de12190ee
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-14 15:53:09 +0900

    main-view-fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   # before_action :authenticate_user!, only: [:index]
   def index
-    @articles = Article.all
+    @articles = Article.all.order("created_at DESC")
+    if user_signed_in?
+      gon.current_id = current_user.id
+    end
   end
 end
