cb1e79b5be77044a715f747ef2178bab86acddc1
jiko797torayo <oec7dwayne7kj@gmail.com>
2018-09-14 15:53:31 +0900

    add-dwayne-function

--- app/assets/javascripts/dwayne.js
+++ app/assets/javascripts/dwayne.js
@@ -1,5 +1,12 @@
 $(function() {
-    $(".brand-logo").on("click", function () {
-        $(".dwayne").addClass("go");
+    $(".action-btn").on("click", function () {
+        var c_id = gon.current_id;
+        if(c_id != 1) {
+            $(".dwayne").addClass("go");
+            return false;
+        }
+    });
+    $(".dwayne2").on("click", function () {
+        $(".dwayne2").addClass("hide");
     });
 });
